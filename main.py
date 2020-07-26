7368205b3165a2d386d6472ef59da3f07311d9ef
The-Spine <dtorkelson@gmail.com>
2020-07-26 09:25:25 -0400

    move main script to root directory

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
+#!/Users/dtork/PycharmProjects/double-slit/venv/bin/python3
 from elasticsearch import Elasticsearch
-
 from src.sample import SampleFlagGenerator
+import random
 
 
 def main():
@@ -9,7 +9,9 @@ def main():
         {'host': 'synapse', 'port': '9200'}
     ])
 
-    for sample_flag in SampleFlagGenerator(1000):
+    random.seed()
+    iterations = random.randint(10, 500)
+    for sample_flag in SampleFlagGenerator(iterations):
         sample_flag.push_to_es(es)
 
 
